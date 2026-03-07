ffb63a55da0a06ff61e5ce19beea27bbeabaaa73
KH-Tan <tankonghwi@gmail.com>
2026-03-07 18:27:21 +0800

    c1

---
+++ Sources/ztpLibNet/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ztpLibNet
+//
+//  Created by Kong Hwi Tan on 7/3/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+      Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
